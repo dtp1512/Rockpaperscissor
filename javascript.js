baeddd5ececd8c37d5831c8985089e63d86891f3
David Pierce <126893864+dtp1512@users.noreply.github.com>
2023-04-08 02:18:03 -0400

    Attempting to set up a loop for the game

--- javascript.js
+++ javascript.js
@@ -6,7 +6,6 @@ function askUser(){
     const choice = prompt("Choose rock, paper, or scissors");
     playerchoice = choice.toLowerCase();
     if (playerchoice == "rock" || playerchoice == "paper" || playerchoice == "scissors") {
-        alert("Thank you");
     } else if (playerchoice == "") {
         alert("Please choose");
         askUser();
@@ -16,9 +15,11 @@ function askUser(){
     }
 
     return playerchoice;
+    
 }
 
 // computer choice //
+let computerChoice;
 
 getRandomInt();
 
@@ -30,28 +31,57 @@ getComputerChoice();
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
-    const computerChoice = choices[getRandomInt(3)];
+    computerChoice = choices[getRandomInt(3)];
 
-    return (computerChoice);
+    
+    return computerChoice;
 }
 
 
 
 //playround - probably need a "for loop" //
 
-//playRound()
 
-function playRound() {
-    if (playerchoice == "rock" && getComputerChoice() == "scissors") {
-       alert("You win! Rock beats Scissors");
-    } else if (playerchoice == "scissors" && getComputerChoice() == "paper") {
-        alert("You win! Scissors beats Paper");
-    } else if (playerchoice == "paper" && getComputerChoice() == "rock") {
-        alert("You win! Paper beats Rock");
-    } else if (playerchoice == getComputerChoice()) {
-        alert("Tie! Play again!");
-    } else {
-            alert("You lose!");
-   }
+let i = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+
+for (let i = 0; i < 5; i++) {
+    function playRound() {
+
+        if (playerchoice == "rock" && computerChoice == "scissors") {
+        alert("You win! Rock beats Scissors");
+        playerScore++
+        } else if (playerchoice == "scissors" && computerChoice == "paper") {
+            alert("You win! Scissors beats Paper");
+            playerScore++
+        } else if (playerchoice == "paper" && computerChoice == "rock") {
+            alert("You win! Paper beats Rock");
+            playerScore++
+        } else if (playerchoice == computerChoice) {
+            alert("Tie! Play again!");
+        } else {
+                alert("You lose!");
+                computerScore++
+    }
+}
 }
 
+
+
+
+
+
+ console.log("Player Score =" + " " + playerScore);
+ console.log("Computer Score =" + " " + computerScore);
+
+ //score()
+
+ //function score() {
+ //   if (playerScore == 1) {
+  //      alert("YOU WIN THE GAME!!!");
+ //   } else {
+ //      alert("YOU LOSE!");
+ //   }
+// }
