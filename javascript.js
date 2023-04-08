57d7bb3e24369e7fc93880572817b5bdcf7bfb59
David Pierce <126893864+dtp1512@users.noreply.github.com>
2023-04-08 00:40:38 -0400

    added computer input and began working on playround section

--- javascript.js
+++ javascript.js
@@ -5,18 +5,57 @@ askUser();
 
 function askUser(){
     const choice = prompt("Choose rock, paper, or scissors");
-    const lowerchoice = choice.toLowerCase();
-    if (lowerchoice == "rock" || lowerchoice == "paper" || lowerchoice == "scissors") {
+    const playerchoice = choice.toLowerCase();
+    if (playerchoice == "rock" || playerchoice == "paper" || playerchoice == "scissors") {
         alert("Thank you");
-        console.log (lowerchoice);
-    } else if (lowerchoice == "") {
+    } else if (playerchoice == "") {
         alert("Please choose");
         askUser();
     } else {
         alert("try again");
         askUser();
     }
-    return lowerchoice;
+
+    return playerchoice;
+}
+
+// computer choice //
+
+getRandomInt();
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+getComputerChoice();
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[getRandomInt(3)];
+
+    return (computerChoice);
+}
+
+
+
+//playround - probably need a "for loop" //
+
+playRound()
+
+function playRound() {
+    if (askUser == "rock" && getComputerChoice() == "scissors") {
+       alert("You win! Rock beats Scissors");
+    } else if (askUser == "scissors" && getComputerChoice() == "paper") {
+        alert("You win! Scissors beats Paper");
+    } else if (askUser == "paper" && getComputerChoice() == "rock") {
+        alert("You win! Paper beats Rock");
+    } else if (askUser == getComputerChoice()) {
+        alert("Tie! Play again!");
+    } else {
+            alert("You lose!");
+   }
+
+
 }
 
 
